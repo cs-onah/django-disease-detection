d8a9deac2339eb3e85d9de4d476a90f003f6ae3c
Chukwuebuka Onah <revelationjay02@gmail.com>
2025-04-05 09:10:33 +0100

    optimize logic

--- myapp/views.py
+++ myapp/views.py
@@ -10,7 +10,7 @@ from PIL import Image
 from tensorflow.keras.preprocessing import image
 
 def mobilenet_prepare_image(file):
-  img = image.load_img(file, target_size = (224,224))
+  img = file.resize((224, 224)) # pass an Image
   img_array = image.img_to_array(img)
   # print(img_array.shape)
   img_array_expanded = np.expand_dims(img_array,axis=0)
@@ -18,7 +18,7 @@ def mobilenet_prepare_image(file):
   return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded)
   
 def mobilenetV2_prepare_image(file):
-  img = image.load_img(file, target_size = (224,224))
+  img = file.resize((224, 224)) # pass an Image
   img_array = image.img_to_array(img)
   # print(img_array.shape)
   img_array_expanded = np.expand_dims(img_array,axis=0)
@@ -35,7 +35,7 @@ def image_upload(request):
         # Assuming your form has an input field named 'image'
         uploaded_image = request.FILES.get('image')
         if uploaded_image:
-            prepare_func, process_func =  mobilenetV2_prepare_image, tf.keras.applications.mobilenet_v2.preprocess_input
+            prepare_func =  mobilenetV2_prepare_image
             model_path = "myapp/model/MobilenetV2.h5"
             model = os.path.join(model_path)
             classes_dict = {0: 'Mosaic_N', 1: 'blight_N', 2: 'brownstreak_N', 3: 'greenmite_N'}
@@ -43,18 +43,13 @@ def image_upload(request):
 
 
             # Open the uploaded image using PIL
-            value = Image.open(uploaded_image)
-            file = value.convert('RGB')
-            temp_file_path = 'image.jpg'
-            file.save(temp_file_path)
+            file = Image.open(uploaded_image)
 
-
-            pixels = prepare_func(temp_file_path)
+            pixels = prepare_func(file)
             pred = model.predict(pixels)
             probs = pred[0].tolist()
 
             response = display(classes_dict,probs)
-            os.remove(temp_file_path) # delete temp file
 
             file.close()
             return JsonResponse(response)
